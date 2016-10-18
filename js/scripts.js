72f40aeac36804c791c683e671348b7dc51b1417
Ryan Mathisen and Tracie Weitzman <tracie@>
2016-10-18 10:05:56 -0700

    add black and white backgrounds

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("button#white").click(function() {
+    $("body").removeClass();
+    // $("body").addClass("white-background");
+  });
+
+  $("button#black").click(function() {
+    $("body").removeClass();
+    $("body").addClass("black-background");
+  });
+});
